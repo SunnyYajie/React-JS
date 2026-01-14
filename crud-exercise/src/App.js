0f551ee3a2dcf4d18af0e86bf7a55f306805e535
SunnyYajie <131513384+SunnyYajie@users.noreply.github.com>
2026-01-14 22:07:08 +0800

    CRUD Exercise: 'Complete' button and some css changes

--- crud-exercise/src/App.js
+++ crud-exercise/src/App.js
@@ -4,6 +4,8 @@ import { useState } from 'react';
 function App() {
   const [todoList, setList] = useState([]);
   const [newTodo, setNewTodo] = useState("");
+  const [isCompleted, setCompleted] = useState(false);
+
   const currentInput = (event) => {
     setNewTodo(event.target.value);
   };
@@ -12,18 +14,23 @@ function App() {
     const todo = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
       todoName: newTodo,
+      completed: isCompleted
     };
     setList([...todoList, todo]);
     setNewTodo("");
   };
   const deleteTodo = (id) => {
-    setList(todoList.filter((todo) => todo.id !== id ));
+    setList(todoList.filter((todo) => todo.id !== id));
   };
+  const completeTodo = (id) => {
+    setList(prev => prev.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
+  };
+
 
   return (
     <div className="App">
-      <div style={{backgroundColor: "#213C51"}}>
-        <h1 style={{ margin: "0rem", padding: ".5rem" , color: "white"}}>To Do List</h1>
+      <div style={{ backgroundColor: "#213C51" }}>
+        <h1 style={{ margin: "0rem", padding: ".5rem", color: "white" }}>To Do List</h1>
       </div>
       <div className="editTodo">
         <input onChange={currentInput} value={newTodo} type="text" /> <br></br>
@@ -32,11 +39,17 @@ function App() {
         <button>Remove Task</button>
       </div>
       <div className='todo'>
-        {todoList.map((todo) => { 
-          return <div className="listItem">
-            <li>{todo.todoName}</li> 
-            <button onClick={() => deleteTodo(todo.id)} style={{alignItems: "right", marginLeft: "1rem", marginBottom: "1rem"}}>Remove</button>
-          </div>; })}
+        {todoList.map((todo) => {
+          return <div className={todo.completed ? "completed" : ""} style={{ display: "flex" }}>
+            <span className="todo-text"><h1 key={todo.id} className={todo.completed ? "completed" : ""}>{todo.todoName}</h1></span>
+
+            <div className="todo-actions">
+              <button onClick={() => deleteTodo(todo.id)} style={{ alignItems: "right", margin: ".5rem"}}>Remove</button>
+              <button onClick={() => completeTodo(todo.id)} style={{ alignItems: "right", margin: ".5rem"}}>Complete</button>
+            </div>
+
+          </div>;
+        })}
       </div>
     </div>
   );
