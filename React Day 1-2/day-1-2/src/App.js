eabdbaee7b59885efcfcf2973db44fa009b0159a
SunnyYajie <131513384+SunnyYajie@users.noreply.github.com>
2026-02-03 22:12:14 +0800

    Day 11 react-hook-form and yup part 1

--- React Day 1-2/day-1-2/src/App.js
+++ React Day 1-2/day-1-2/src/App.js
@@ -1,43 +1,11 @@
 import styles from './App.module.css';
-import Axios from 'axios';
-import { useState, useEffect, createContext } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useHistory, useLocation } from 'react-router-dom';
-import { Home } from "./pages/Home";
-import { Menu } from "./pages/Menu";
-import { Contact } from "./pages/Contact";
-import { Profile } from "./pages/Profile";
-import { Navbar } from "./pages/Navbar";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-export const AppContext = createContext();
+import { Form } from './pages/Form';
 
 
 function App() {
-  const client = new QueryClient({defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    }
-  }});
-
-
-  const [username, setUsername] = useState("Minion");
-
   return (
     <div className={styles.App}>
-      <QueryClientProvider client={client}>
-        <AppContext.Provider value={{ username, setUsername }}>
-          <Router>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/menu" element={<Menu />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="*" element={<h1>Page not found</h1>} />
-            </Routes>
-          </Router>
-        </AppContext.Provider>
-      </QueryClientProvider>
+      <Form />
     </div>
   );
 }
---
+++ React Day 1-2/day-1-2/src/pages/Form.js
@@ -0,0 +1,33 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup';
+
+export const Form = () => {
+    const schema = yup.object().shape({
+        fullName: yup.string().required(),
+        email: yup.string().email().required(),
+        age: yup.number().positive().integer().min(12).required(),
+        password: yup.string().min(3).max(10).required(),
+        confirmPassword: yup.string().oneOf([yup.ref("password"), null]).required(),
+    });
+
+    const {register, handleSubmit} = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Full Name" {...register("fullName")} />
+            <input type="email" placeholder="Email" {...register("email")}/>
+            <input type="number" placeholder="Age" {...register("age")}/>
+            <input type="password" placeholder="Password" {...register("password")}/>
+            <input type="password" placeholder="Confirm Password" {...register("confirmPassword")}/>
+            <input type="submit" />
+        </form>
+    );
+
+};
